a0cd388b7b0666a12fbd54ca0ffdea8020907778
jchancellor-ms <jchancellor@microsoft.com>
2022-11-22 20:02:40 -0800

    variables clean-up

--- modules/avs_vmware_calling_module_create_vm_and_network_segment/variables.tf
+++ modules/avs_vmware_calling_module_create_vm_and_network_segment/variables.tf
@@ -1,15 +1,8 @@
-variable "deployment" {
-  description = "Map object containing all of the input values for the module"
-}
-
-variable "vsphere_creds" {
-  description = "Map object containing the vsphere login credentials"
-  sensitive   = true
-}
 variable "vmware_deployment" {
   description = "map of values for the terraform modules being built on vsphere and nsx"
 }
 
 variable "vmware_creds" {
   description = "map of credential values for the terraform provider"
+  sensitive   = true
 }
